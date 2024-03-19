f4128c250ee012fdb4e069bf74d17e4cf8a73984
Степан Бабаков <stepan.babakov.1999@mail.ru>
2024-03-19 17:08:48 +0300

    test project pt.2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .use(VueAxios, axios)
+  .mount('#app')
